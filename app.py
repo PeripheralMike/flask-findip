78d02745ec6e3c9c53eacc0e177657065eff12a5
Mike <mikesmith@screamingjoypad.com>
2020-08-19 12:07:40 +0100

    cleaned up spelling in code comments

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 # app.py
-from flask import Flask           # import flask
-import ipgetter2
-from ipgetter2 import ipgetter1 as ipgetter
-myip = ipgetter.myip()
+from flask import Flask                     # imports flask
+import ipgetter2                            # imports ipgetter2
+from ipgetter2 import ipgetter1 as ipgetter # uses the functions of the original ipgetter
+myip = ipgetter.myip()                      #sets the variable of 'myip' to the external ip address
 
-app = Flask(__name__)
-@app.route("/")                   # at the end point /
-def hello():                      # call method hello
-    return "" + myip      # which returns "hello world"s
+app = Flask(__name__)                       # generated the flask app
+@app.route("/")                             # at the end point /
+def hello():                                # call method hello
+    return "" + myip                        # which returns the external ip address
 
-if __name__ == "__main__":        # on running python app.py
-    app.run(debug=True)                     # run the flask app
+if __name__ == "__main__":                  # on running python app.py
+    app.run()                               # run the flask app (add debug=True to run() to enter debug mode)
