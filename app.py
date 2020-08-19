d6f6e73687b2ae007db1757eaadfae28f71d95ee
Mike <mikesmith@screamingjoypad.com>
2020-08-19 11:05:34 +0100

    works using an external service urllib and ident.me

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 # app.py
 from flask import Flask           # import flask
 app = Flask(__name__)             # create an app instance
-ip = "this is the ip address 1.1.1.1"
+import urllib.request
+external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
 
 @app.route("/")                   # at the end point /
 def hello():                      # call method hello
-    return "" + ip         # which returns "hello world"s
+    return "" + external_ip         # which returns "hello world"s
 
 
 if __name__ == "__main__":        # on running python app.py
