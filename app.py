20de46b244fec3771a7026900bd9ae1974d506cb
Mike <mikesmith@screamingjoypad.com>
2020-08-19 11:24:16 +0100

    used os.requests to find the IP in the header as a solution

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 # app.py
 from flask import Flask           # import flask
 app = Flask(__name__)             # create an app instance
-import urllib.request
-external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+import requests
+
+myip = requests.get('https://www.google.com').headers['X-Client-IP']
 
 @app.route("/")                   # at the end point /
 def hello():                      # call method hello
-    return "" + external_ip         # which returns "hello world"s
-
+    return "" + myip       # which returns "hello world"s
 
 if __name__ == "__main__":        # on running python app.py
     app.run(debug=True)                     # run the flask app
