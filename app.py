c3bfe606dbd52398d9310db6fc3ddbef9db622f7
Mike <mikesmith@screamingjoypad.com>
2020-08-19 11:00:49 +0100

    Getting the site to reproduce a variable

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # app.py
 from flask import Flask           # import flask
 app = Flask(__name__)             # create an app instance
-
+ip = "this is the ip address 1.1.1.1"
 
 @app.route("/")                   # at the end point /
 def hello():                      # call method hello
-    return "Hello World!"         # which returns "hello world"
+    return "" + ip         # which returns "hello world"s
 
 
 if __name__ == "__main__":        # on running python app.py
-    app.run()                     # run the flask app
+    app.run(debug=True)                     # run the flask app
