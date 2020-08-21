2656b4e0bc2f21ea72803cb6bb3925e3fd0b0022
Mike <mikesmith@screamingjoypad.com>
2020-08-21 10:09:59 +0100

    added host and port information for hosting

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def hello():                                # call method hello
     return "" + myip                        # which returns the external ip address
 
 if __name__ == "__main__":                  # on running python app.py
-    app.run()                               # run the flask app (add debug=True to run() to enter debug mode)
+    app.run(host='0.0.0.0', port=8080)                               # run the flask app (add debug=True to run() to enter debug mode)
